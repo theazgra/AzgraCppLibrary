48c467ea10f81122bc0f6a734f1f5ad0c5e30b05
Vojtěch Moravec <theazgra@gmail.com>
2019-09-25 10:34:49 +0200

    New functions in our `enumerable`/

--- include/azgra/collection/linq.h
+++ include/azgra/collection/linq.h
@@ -2,14 +2,39 @@
 
 #include <vector>
 #include <functional>
+#include <boost/optional.hpp>
+
+#define REQUIRE_NUMERIC_TYPE static_assert(                         \
+                                std::is_same<T, int>() ||           \
+                                std::is_same<T, long>() ||          \
+                                std::is_same<T, size_t>() ||        \
+                                std::is_same<T, float>() ||         \
+                                std::is_same<T, double>() ||        \
+                                std::is_same<T, azgra::byte>() ||   \
+                                std::is_same<T, azgra::u16>() ||    \
+                                std::is_same<T, azgra::u32>() ||    \
+                                std::is_same<T, azgra::u64>() ||    \
+                                std::is_same<T, azgra::i16>() ||    \
+                                std::is_same<T, azgra::i32>() ||    \
+                                std::is_same<T, azgra::i64>(),      \
+"Type must be numeric.")
+
 
 namespace azgra::collection::experimental_linq
 {
+    class EnumerableError : public std::runtime_error
+    {
+    public:
+        explicit EnumerableError(const char *errorMessage) : std::runtime_error(errorMessage)
+        {}
+    };
+
     template<typename T>
     class Enumerable
     {
         template<typename U>
-        friend class Enumerable;
+        friend
+        class Enumerable;
 
     private:
         std::vector<T> data;
@@ -17,13 +42,18 @@ namespace azgra::collection::experimental_linq
     public:
         Enumerable() = default;
 
+        explicit Enumerable(std::vector<T> dataSrc)
+        {
+            data = std::vector<T>(std::move(dataSrc));
+        }
+
         template<typename CopyIt>
         Enumerable(CopyIt srcBegin, CopyIt srcEnd)
         {
             data = std::vector<T>(srcBegin, srcEnd);
         }
 
-        Enumerable<T> where(std::function<bool(const T &)> predicate)
+        Enumerable<T> where(std::function<bool(const T &)> predicate) const noexcept
         {
             Enumerable result;
             for (const T &item : data)
@@ -37,7 +67,7 @@ namespace azgra::collection::experimental_linq
         }
 
         template<typename SelectType>
-        Enumerable<SelectType> select(std::function<SelectType(const T &)> selector)
+        Enumerable<SelectType> select(std::function<SelectType(const T &)> selector) const noexcept
         {
             Enumerable<SelectType> result;
             result.data.resize(data.size());
@@ -48,6 +78,241 @@ namespace azgra::collection::experimental_linq
             }
             return result;
         }
+
+        T first() const noexcept(false)
+        {
+            if (data.size() > 0)
+            {
+                return data[0];
+            }
+            else
+            {
+                throw EnumerableError("Enumerable is empty.");
+            }
+        }
+
+
+        boost::optional<T> first_or_default() const noexcept
+        {
+            if (data.size() > 0)
+            {
+                return data[0];
+            }
+            else
+            {
+                return boost::none;
+            }
+        }
+
+        T last() const noexcept(false)
+        {
+            if (data.size() > 0)
+            {
+                return data[data.size() - 1];
+            }
+            else
+            {
+                throw EnumerableError("Enumerable is empty.");
+            }
+        }
+
+
+        boost::optional<T> last_or_default() const noexcept
+        {
+            if (data.size() > 0)
+            {
+                return data[data.size() - 1];
+            }
+            else
+            {
+                return boost::none;
+            }
+        }
+
+        T first(std::function<bool(const T &)> predicate) const noexcept(false)
+        {
+            if (data.size() > 0)
+            {
+                for (const T &item : data)
+                {
+                    if (predicate(item))
+                    {
+                        return item;
+                    }
+                }
+                throw EnumerableError("Didn't find requested item.");
+            }
+            else
+            {
+                throw EnumerableError("Enumerable is empty.");
+            }
+        }
+
+
+        boost::optional<T> first_or_default(std::function<bool(const T &)> predicate) const noexcept
+        {
+            if (data.size() > 0)
+            {
+                for (const T &item : data)
+                {
+                    if (predicate(item))
+                    {
+                        return item;
+                    }
+                }
+            }
+            return boost::none;
+        }
+
+        T single(std::function<bool(const T &)> predicate) const noexcept(false)
+        {
+            T result;
+            bool found = false;
+            if (data.size() > 0)
+            {
+                for (const T &item : data)
+                {
+                    if (predicate(item))
+                    {
+                        if (found)
+                        {
+                            throw EnumerableError("More than one item satisfy the condition.");
+                        }
+                        result = item;
+                        found = true;
+                    }
+                }
+                if (found)
+                {
+                    return result;
+                }
+                else
+                {
+                    throw EnumerableError("No item satisfies the condition.");
+                }
+            }
+            else
+            {
+                throw EnumerableError("Enumerable is empty.");
+            }
+        }
+
+        boost::optional<T> single_or_default(std::function<bool(const T &)> predicate) const noexcept(false)
+        {
+            T result;
+            bool found = false;
+            if (data.size() > 0)
+            {
+                for (const T &item : data)
+                {
+                    if (predicate(item))
+                    {
+                        if (found)
+                        {
+                            throw EnumerableError("More than one item satisfy the condition.");
+                        }
+                        result = item;
+                        found = true;
+                    }
+                }
+                if (found)
+                {
+                    return result;
+                }
+                else
+                {
+                    return boost::none;
+                }
+            }
+            else
+            {
+                return boost::none;
+            }
+        }
+
+        bool any() const noexcept
+        {
+            return data.size() > 0;
+        }
+
+        bool any(std::function<bool(const T &)> predicate) const noexcept
+        {
+            if (data.size() > 0)
+            {
+                for (const T &item : data)
+                {
+                    if (predicate(item))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        bool all(std::function<bool(const T &)> predicate) const noexcept
+        {
+            for (const T &item : data)
+            {
+                if (!predicate(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        size_t count() const noexcept
+        {
+            return data.size();
+        }
+
+        size_t count(std::function<bool(const T &)> predicate) const noexcept
+        {
+            size_t result = 0;
+            for (const T &item : data)
+            {
+                if (predicate(item))
+                {
+                    ++result;
+                }
+            }
+            return result;
+        }
+
+        bool contains(std::function<bool(const T &)> predicate) const noexcept
+        {
+            for (const T &item : data)
+            {
+                if (predicate(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        double average() const
+        {
+            REQUIRE_NUMERIC_TYPE;
+            double sum = 0.0;
+            size_t count = 0;
+            for (const T &item : data)
+            {
+                sum += item;
+                ++count;
+            }
+
+            return (sum / static_cast<double>(count));
+        }
+
+
+
+        // std::function<bool(const T &)> predicate
+        // std::function<SelectType(const T &)> selector
+        // TODO:  Distinct, Concat, Except, OrderBy, OrderByDescending, Reverse, Repeat, Sum, Take, Skip, TakeWhile, TakeLast, ForEach,
+        //  Cast?
     };
 
 
@@ -65,6 +330,5 @@ namespace azgra::collection::experimental_linq
         return result;
     }
 
-    // TODO: First,FirstOrDefault,Single,SingleOrDefault,Any,All,ForEach,Count,Count(predicate),Cast?
 
 }
--- test.cpp
+++ test.cpp
@@ -22,6 +22,7 @@ struct Number
 
 int main(int, char **)
 {
+
     std::vector<Number> nums = {Number(0), Number(1), Number(2), Number(3), Number(4), Number(5), Number(6), Number(7), Number(8)};
 //    std::set<Number> nums = {Number(0), Number(1), Number(2), Number(3), Number(4), Number(5), Number(6), Number(7), Number(8)};
 //    auto x = where(nums.begin(), nums.end(), static_cast<std::function<bool(const Number &)>>([](const Number &x)
@@ -31,9 +32,22 @@ int main(int, char **)
     std::function<bool(const Number &)> even = [](const Number &n)
     { return (n.n % 2 == 0); };
     Enumerable<Number> en(nums.begin(), nums.end());
+    Enumerable<Number> emptyEn;
 
-    std::function<int(const Number &)> selectInts = [](const Number &n) { return n.n; };
-    auto tmp = en.select(selectInts);
+    try
+    {
+        Number first = emptyEn.first();
+        fprintf(stdout, "First value is %i\n", first.n);
+    }
+    catch (azgra::collection::experimental_linq::EnumerableError &e)
+    {
+        fprintf(stderr, "%s\n", e.what());
+    }
+    std::vector<azgra::u16> vec={5,45,89,78,64,21,45,45,4};
+    Enumerable<azgra::u16> xxx(vec);
+
+    double avg = xxx.average();
+
 
     return 0;
 }
