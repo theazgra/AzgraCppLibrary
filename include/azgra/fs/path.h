ab49b52e9ef3c15704261014cb861e3ef6b8d48e
Vojtěch Moravec <theazgra@gmail.com>
2019-11-24 22:48:25 +0100

    Added comments.

--- include/azgra/fs/path.h
+++ include/azgra/fs/path.h
@@ -11,17 +11,39 @@ namespace azgra::fs
         Path() = default;
 
     public:
-        [[nodiscard]] static sfs::path concat_path(const std::vector<BasicStringView < char>>
-
-        &paths);
-
+        /**
+         * Check if file specified by path is regular file.
+         * @param path Requested path.
+         * @return True if path points to regular file.
+         */
         [[nodiscard]] static bool is_regular_file(const BasicStringView<char> &path);
 
+        /**
+         * Check if file specified by path is directory.
+         * @param path Requested path.
+         * @return True if path points to directory.
+         */
         [[nodiscard]] static bool is_directory(const BasicStringView<char> &path);
 
+        /**
+         * Get path to the temporary directory.
+         * @return Temporary directory path.
+         */
         [[nodiscard]] static sfs::path get_temporary_directory_path();
 
+        /**
+         * Get path to the temporary file.
+         * @return Temporary file path.
+         */
         [[nodiscard]] static sfs::path get_temporary_file_path();
 
+        typedef std::vector<BasicStringView < char>> VectorOfStringViews;
+
+        /**
+         * Perform std::filesystem path concatenation on strings.
+         * @param paths Strings to be concatenated.
+         * @return Resulted path.
+         */
+        [[nodiscard]] static sfs::path concat_path(const VectorOfStringViews &paths);
     };
 }
