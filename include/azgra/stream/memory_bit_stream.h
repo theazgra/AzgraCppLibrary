59ffbfce75b636c9a3705ac0e9fc5bc9bd1617ea
Vojtech Moravec <vojtech.moravec.st@vsb.cz>
2019-11-25 14:45:26 +0100

    Fixed write_value/read_value in BitStream

--- include/azgra/stream/memory_bit_stream.h
+++ include/azgra/stream/memory_bit_stream.h
@@ -1,9 +1,10 @@
 #pragma once
+
 #include <azgra/azgra.h>
 
 namespace azgra
 {
-constexpr size_t MAX_BIT_COUNT = 255;
+    constexpr size_t MAX_BIT_COUNT = 255;
 // if MSB_FIRST is true, than MSB is written first to the stream when writing value.
 #define LSB_FIRST 0
 
@@ -13,178 +14,195 @@ constexpr size_t MAX_BIT_COUNT = 255;
  * There is bit access and byte access (write_bytes_no_alloc, read_value(byteCount)), one must not mix those.
  * If there would be call to both of those the internal buffer could be mixed and value would be corrupted.
  */
-class OutMemoryBitStream
-{
-  private:
-    // Memory buffer.
-    ByteArray buffer;
-    // Index to the next write in memory buffer.
-    size_t memoryBufferIndex = 0;
-    // Bit buffer.
-    byte bitBuffer;
-    // Actual size of bit buffer.
-    byte bitBufferSize = 0;
-    // Number of bits to which value will be encoded when `write_value` is called.
-    size_t valueEncodeBitCount = 0;
-
-    void internal_write_bit(const bool &bit, const bool &alloc);
-    void internal_flush_bit_buffer(const bool &alloc);
-
-  public:
-    OutMemoryBitStream(size_t valueEncodeBitCount = 1);
-    ~OutMemoryBitStream();
-    // Resize the memory buffer for `write_value_no_alloc` and `write_bit_no_alloc`
-    void resize_for_raw_write(const size_t size);
-
-    // Write bit to memory stream.
-    void write_bit(const bool &bit);
-    // Write bit without resizing buffer. Buffer must be allocated using `resize_for_raw_write` !
-    void write_bit_no_alloc(const bool &bit);
-    // Write bit to memory stream.
-    void operator<<(const bool &bit);
-
-    // If flush buffer is not flushed, flush it to main buffer and clear it.
-    void flush_bit_buffer();
-    // Get buffer of written bits, which may not be flushed.
-    ByteArray get_buffer() const;
-    // Get buffer of written bits, which is flushed.
-    ByteArray get_flushed_buffer();
-    // Get buffer of written bits, which is flushed. For allocated buffer.
-    ByteArray get_flushed_buffer_no_alloc();
-
-    template <typename T>
-    void write_value(const T &value)
+    class OutMemoryBitStream
     {
-        // Lets do it Little-Endian way, start with LSB and move to MSB.
-        bool bit;
-#if LSB_FIRST
-        for (size_t bitPos = 0; bitPos < valueEncodeBitCount; bitPos++)
-        {
-            bit = value & (1 << bitPos);
-            write_bit(bit);
-        }
-#else
-        // Start with MSB.
-        for (size_t bitPos = (valueEncodeBitCount - 1); bitPos >= 0; bitPos--)
-        {
-            bit = value & (1 << bitPos);
-            write_bit(bit);
-        }
-#endif
-    }
+    private:
+        // Memory buffer.
+        ByteArray buffer;
+        // Index to the next write in memory buffer.
+        size_t memoryBufferIndex = 0;
+        // Bit buffer.
+        byte bitBuffer;
+        // Actual size of bit buffer.
+        byte bitBufferSize = 0;
+        // Number of bits to which value will be encoded when `write_value` is called.
+        size_t valueEncodeBitCount = 0;
 
-    // Write value without resizing buffer. Buffer must be allocated using `resize_for_raw_write` !
-    template <typename T>
-    void write_value_no_alloc(const T &value)
-    {
-        // Lets do it Little-Endian way, start with LSB and move to MSB.
-        bool bit;
-#if LSB_FIRST
-        for (size_t bitPos = 0; bitPos < valueEncodeBitCount; bitPos++)
-        {
-            bit = value & (1 << bitPos);
-            write_bit_no_alloc(bit);
-        }
-#else
-        // Start with MSB.
-        for (azgra::i32 bitPos = (valueEncodeBitCount - 1); bitPos >= 0; bitPos--)
-        {
-            bit = value & (1 << bitPos);
-            write_bit_no_alloc(bit);
-        }
-#endif
-    }
+        void internal_write_bit(const bool &bit, const bool &alloc);
 
-    template <typename T>
-    void write_bytes_no_alloc(const T &value, const azgra::i32 &byteCount)
-    {
+        void internal_flush_bit_buffer(const bool &alloc);
+
+    public:
+        OutMemoryBitStream(size_t valueEncodeBitCount = 1);
+
+        ~OutMemoryBitStream();
+
+        // Resize the memory buffer for `write_value_no_alloc` and `write_bit_no_alloc`
+        void resize_for_raw_write(const size_t size);
+
+        // Write bit to memory stream.
+        void write_bit(const bool &bit);
+
+        // Write whole byte to memory stream.
+        //void write_byte(const byte &b);
+
+        // Write bit without resizing buffer. Buffer must be allocated using `resize_for_raw_write` !
+        void write_bit_no_alloc(const bool &bit);
+
+        // Write bit to memory stream.
+        void operator<<(const bool &bit);
+
+        // If flush buffer is not flushed, flush it to main buffer and clear it.
+        void flush_bit_buffer();
+
+        // Get buffer of written bits, which may not be flushed.
+        ByteArray get_buffer() const;
+
+        // Get buffer of written bits, which is flushed.
+        ByteArray get_flushed_buffer();
+
+        // Get buffer of written bits, which is flushed. For allocated buffer.
+        ByteArray get_flushed_buffer_no_alloc();
+
+        template<typename T>
+        void write_value(const T &value)
+        {
+            // Lets do it Little-Endian way, start with LSB and move to MSB.
+            bool bit;
 #if LSB_FIRST
-        for (size_t byteIndex = 0; byteIndex < byteCount; byteIndex++)
-        {
-            *(buffer.data() + memoryBufferIndex++) = *(((byte *)&value) + byteIndex);
-        }
+            for (size_t bitPos = 0; bitPos < valueEncodeBitCount; bitPos++)
+            {
+                bit = value & (1 << bitPos);
+                write_bit(bit);
+            }
 #else
-        for (azgra::i32 byteIndex = (byteCount - 1); byteIndex >= 0; byteIndex--)
-        {
-            *(buffer.data() + memoryBufferIndex++) = *(((byte *)&value) + byteIndex);
-        }
+            const size_t valueSize = sizeof(T) * 8;
+            // Start with MSB.
+            for (long bitPos = (valueSize - 1); bitPos >= 0; bitPos--)
+            {
+                bit = value & (1 << bitPos);
+                write_bit(bit);
+            }
 #endif
-    }
-};
+        }
+
+        // Write value without resizing buffer. Buffer must be allocated using `resize_for_raw_write` !
+        template<typename T>
+        void write_value_no_alloc(const T &value)
+        {
+            // Lets do it Little-Endian way, start with LSB and move to MSB.
+            bool bit;
+#if LSB_FIRST
+            for (size_t bitPos = 0; bitPos < valueEncodeBitCount; bitPos++)
+            {
+                bit = value & (1 << bitPos);
+                write_bit_no_alloc(bit);
+            }
+#else
+            // Start with MSB.
+            for (azgra::i32 bitPos = (valueEncodeBitCount - 1); bitPos >= 0; bitPos--)
+            {
+                bit = value & (1 << bitPos);
+                write_bit_no_alloc(bit);
+            }
+#endif
+        }
+
+        template<typename T>
+        void write_bytes_no_alloc(const T &value, const azgra::i32 &byteCount)
+        {
+#if LSB_FIRST
+            for (size_t byteIndex = 0; byteIndex < byteCount; byteIndex++)
+            {
+                *(buffer.data() + memoryBufferIndex++) = *(((byte *)&value) + byteIndex);
+            }
+#else
+            for (azgra::i32 byteIndex = (byteCount - 1); byteIndex >= 0; byteIndex--)
+            {
+                *(buffer.data() + memoryBufferIndex++) = *(((byte *) &value) + byteIndex);
+            }
+#endif
+        }
+    };
 
 // Class alowing to read invidual bits from memory buffer.
-class InMemoryBitStream
-{
-  private:
-    // Memory buffer.
-    const ByteArray *memoryBuffer = nullptr;
-    // Position in memory buffer.
-    size_t memoryBufferPosition = 0;
-    // Bit buffer.
-    byte bitBuffer = 0;
-    // Current bit buffer size.
-    byte bitBufferSize = 0;
-    // Number of bits to consume when reading value by `read_value`.
-    size_t valueEncodeBitCount;
-
-    // Read byte into bit buffer.
-    void read_byte_to_bit_buffer();
-
-  public:
-    InMemoryBitStream(const ByteArray *buffer, size_t bufferPosition = 0, size_t valueEncodeBitCount = 1);
-    ~InMemoryBitStream();
-
-    // Read one bit from memory buffer.
-    bool read_bit();
-    // Read one bit from memory buffer.
-    void operator>>(bool &bit);
-    // Check wheter atleast one bit can be read.
-    bool can_read() const;
-
-    template <typename T>
-    T read_value()
+    class InMemoryBitStream
     {
-        bool bit;
-        T result = 0;
-#if LSB_FIRST
-        for (size_t bitPos = 0; bitPos < valueEncodeBitCount; bitPos++)
-        {
-            bit = read_bit();
-            result |= (bit << bitPos);
-        }
-#else
-        for (azgra::i32 bitPos = (valueEncodeBitCount - 1); bitPos >= 0; bitPos--)
-        {
-            bit = read_bit();
-            result |= (bit << bitPos);
-        }
-#endif
-        return result;
-    }
+    private:
+        // Memory buffer.
+        const ByteArray *memoryBuffer = nullptr;
+        // Position in memory buffer.
+        size_t memoryBufferPosition = 0;
+        // Bit buffer.
+        byte bitBuffer = 0;
+        // Current bit buffer size.
+        byte bitBufferSize = 0;
+        // Number of bits to consume when reading value by `read_value`.
+        size_t valueEncodeBitCount;
 
-    template <typename T>
-    T read_value(const azgra::i32 &byteCount)
-    {
-        always_assert(memoryBufferPosition + (byteCount - 1) < memoryBuffer->size());
-        T result = 0;
-        // bitBuffer = (*memoryBuffer)[memoryBufferPosition++];
+        // Read byte into bit buffer.
+        void read_byte_to_bit_buffer();
+
+    public:
+        InMemoryBitStream(const ByteArray *buffer, size_t bufferPosition = 0, size_t valueEncodeBitCount = 1);
+
+        ~InMemoryBitStream();
+
+        // Read one bit from memory buffer.
+        bool read_bit();
+
+        // Read one bit from memory buffer.
+        void operator>>(bool &bit);
+
+        // Check wheter atleast one bit can be read.
+        bool can_read() const;
+
+        template<typename T>
+        T read_value()
+        {
+            bool bit;
+            T result = 0;
 #if LSB_FIRST
-        for (size_t byteIndex = 0; byteIndex < byteCount; byteIndex++)
-        {
-            result |= (memoryBuffer->operator[](memoryBufferPosition++)) << (byteIndex * 8);
-        }
+            for (size_t bitPos = 0; bitPos < valueEncodeBitCount; bitPos++)
+            {
+                bit = read_bit();
+                result |= (bit << bitPos);
+            }
 #else
-        for (azgra::i32 byteIndex = (byteCount - 1); byteIndex >= 0; byteIndex--)
-        {
-            result |= (memoryBuffer->operator[](memoryBufferPosition++)) << (byteIndex * 8);
-        }
+            const size_t valueSize = sizeof(T) * 8;
+            for (long bitPos = (valueSize - 1); bitPos >= 0; bitPos--)
+            {
+                bit = read_bit();
+                result |= (bit << bitPos);
+            }
 #endif
-        return result;
-    }
-};
+            return result;
+        }
+
+        template<typename T>
+        T read_value(const azgra::i32 &byteCount)
+        {
+            always_assert(memoryBufferPosition + (byteCount - 1) < memoryBuffer->size());
+            T result = 0;
+            // bitBuffer = (*memoryBuffer)[memoryBufferPosition++];
+#if LSB_FIRST
+            for (size_t byteIndex = 0; byteIndex < byteCount; byteIndex++)
+            {
+                result |= (memoryBuffer->operator[](memoryBufferPosition++)) << (byteIndex * 8);
+            }
+#else
+            for (azgra::i32 byteIndex = (byteCount - 1); byteIndex >= 0; byteIndex--)
+            {
+                result |= (memoryBuffer->operator[](memoryBufferPosition++)) << (byteIndex * 8);
+            }
+#endif
+            return result;
+        }
+    };
 
 // Find number of bits required to encode value of `maxValue`.
-size_t bits_required(size_t maxValue);
+    size_t bits_required(size_t maxValue);
+
 // Find number of whole bytes required to encode value of `maxValue`.
-size_t bytes_required(size_t maxValue);
+    size_t bytes_required(size_t maxValue);
 } // namespace azgra
--- src/stream/memory_bit_stream.cpp
+++ src/stream/memory_bit_stream.cpp
@@ -6,176 +6,195 @@ namespace azgra
 /******************************************************************************************************************************
   * OutMemoryBitStream implementation
   ****************************************************************************************************************************/
-OutMemoryBitStream::OutMemoryBitStream(size_t valueEncodeBitCount)
-{
-    bitBuffer = 0;
-    bitBufferSize = 0;
-    memoryBufferIndex = 0;
-    this->valueEncodeBitCount = valueEncodeBitCount;
-}
-
-OutMemoryBitStream::~OutMemoryBitStream()
-{
-    flush_bit_buffer();
-    bitBuffer = 0;
-    memoryBufferIndex = 0;
-    bitBufferSize = 0;
-}
-
-void OutMemoryBitStream::resize_for_raw_write(const size_t size)
-{
-    buffer.resize(size);
-}
-
-void OutMemoryBitStream::operator<<(const bool &bit)
-{
-    internal_write_bit(bit, true);
-}
-
-void OutMemoryBitStream::internal_write_bit(const bool &bit, const bool &alloc)
-{
-    ++bitBufferSize;
-    if (bit)
+    OutMemoryBitStream::OutMemoryBitStream(size_t valueEncodeBitCount)
     {
-        bitBuffer |= (1 << (8 - bitBufferSize));
-    }
-
-    if (bitBufferSize == 8)
-    {
-        internal_flush_bit_buffer(alloc);
-    }
-}
-
-void OutMemoryBitStream::write_bit(const bool &bit)
-{
-    internal_write_bit(bit, true);
-}
-
-void OutMemoryBitStream::write_bit_no_alloc(const bool &bit)
-{
-    internal_write_bit(bit, false);
-}
-
-void OutMemoryBitStream::flush_bit_buffer()
-{
-    internal_flush_bit_buffer(true);
-}
-
-void OutMemoryBitStream::internal_flush_bit_buffer(const bool &alloc)
-{
-    if (bitBufferSize > 0)
-    {
-        if (alloc)
-        {
-            buffer.push_back(bitBuffer);
-            ++memoryBufferIndex;
-        }
-        else
-        {
-            buffer[memoryBufferIndex++] = bitBuffer;
-        }
-
         bitBuffer = 0;
         bitBufferSize = 0;
+        memoryBufferIndex = 0;
+        this->valueEncodeBitCount = valueEncodeBitCount;
     }
-}
 
-ByteArray OutMemoryBitStream::get_buffer() const
-{
-    return buffer;
-}
+    OutMemoryBitStream::~OutMemoryBitStream()
+    {
+        flush_bit_buffer();
+        bitBuffer = 0;
+        memoryBufferIndex = 0;
+        bitBufferSize = 0;
+    }
 
-ByteArray OutMemoryBitStream::get_flushed_buffer()
-{
-    internal_flush_bit_buffer(true);
-    return buffer;
-}
+    void OutMemoryBitStream::resize_for_raw_write(const size_t size)
+    {
+        buffer.resize(size);
+    }
 
-ByteArray OutMemoryBitStream::get_flushed_buffer_no_alloc()
-{
-    internal_flush_bit_buffer(false);
-    return buffer;
-}
+    void OutMemoryBitStream::operator<<(const bool &bit)
+    {
+        internal_write_bit(bit, true);
+    }
+
+//    void OutMemoryBitStream::write_byte(const byte &b)
+//    {
+//        if (bitBufferSize == 0)
+//        {
+//            bitBuffer = b;
+//            bitBufferSize = 8;
+//            internal_flush_bit_buffer(true);
+//        }
+//        else
+//        {
+//            for (unsigned int i = 7; i >= 0; --i)
+//            {
+//                const bool bit = (b & (static_cast<byte>(1) << i));
+//                internal_write_bit(bit, true);
+//            }
+//        }
+//    }
+
+    void OutMemoryBitStream::internal_write_bit(const bool &bit, const bool &alloc)
+    {
+        ++bitBufferSize;
+        if (bit)
+        {
+            bitBuffer |= (1 << (8 - bitBufferSize));
+        }
+
+        if (bitBufferSize == 8)
+        {
+            internal_flush_bit_buffer(alloc);
+        }
+    }
+
+    void OutMemoryBitStream::write_bit(const bool &bit)
+    {
+        internal_write_bit(bit, true);
+    }
+
+    void OutMemoryBitStream::write_bit_no_alloc(const bool &bit)
+    {
+        internal_write_bit(bit, false);
+    }
+
+    void OutMemoryBitStream::flush_bit_buffer()
+    {
+        internal_flush_bit_buffer(true);
+    }
+
+    void OutMemoryBitStream::internal_flush_bit_buffer(const bool &alloc)
+    {
+        if (bitBufferSize > 0)
+        {
+            if (alloc)
+            {
+                buffer.push_back(bitBuffer);
+                ++memoryBufferIndex;
+            }
+            else
+            {
+                buffer[memoryBufferIndex++] = bitBuffer;
+            }
+
+            bitBuffer = 0;
+            bitBufferSize = 0;
+        }
+    }
+
+    ByteArray OutMemoryBitStream::get_buffer() const
+    {
+        return buffer;
+    }
+
+    ByteArray OutMemoryBitStream::get_flushed_buffer()
+    {
+        internal_flush_bit_buffer(true);
+        return buffer;
+    }
+
+    ByteArray OutMemoryBitStream::get_flushed_buffer_no_alloc()
+    {
+        internal_flush_bit_buffer(false);
+        return buffer;
+    }
+
 
 /******************************************************************************************************************************
   * InMemoryBitStream implementation
   ****************************************************************************************************************************/
 
-InMemoryBitStream::InMemoryBitStream(const ByteArray *buffer, size_t bufferPosition, size_t valueEncodeBitCount)
-{
-    memoryBuffer = buffer;
-    memoryBufferPosition = bufferPosition;
-    bitBufferSize = 0;
-    bitBuffer = 0;
-    this->valueEncodeBitCount = valueEncodeBitCount;
-}
-
-InMemoryBitStream::~InMemoryBitStream()
-{
-    memoryBuffer = nullptr;
-    memoryBufferPosition = 0;
-    bitBufferSize = 0;
-    bitBuffer = 0;
-}
-
-void InMemoryBitStream::read_byte_to_bit_buffer()
-{
-    if (memoryBufferPosition < memoryBuffer->size())
+    InMemoryBitStream::InMemoryBitStream(const ByteArray *buffer, size_t bufferPosition, size_t valueEncodeBitCount)
     {
-        bitBuffer = memoryBuffer->operator[](memoryBufferPosition++);
-        bitBufferSize = 8;
-    }
-    else
-    {
-        always_assert(false && "Out of memory in buffer");
-    }
-}
-
-bool InMemoryBitStream::read_bit()
-{
-    if (bitBufferSize == 0)
-    {
-        read_byte_to_bit_buffer();
+        memoryBuffer = buffer;
+        memoryBufferPosition = bufferPosition;
+        bitBufferSize = 0;
+        bitBuffer = 0;
+        this->valueEncodeBitCount = valueEncodeBitCount;
     }
 
-    --bitBufferSize;
-    bool result = bitBuffer & (1 << bitBufferSize);
-    return result;
-}
-
-void InMemoryBitStream::operator>>(bool &bit)
-{
-    bit = read_bit();
-}
-
-bool InMemoryBitStream::can_read() const
-{
-    return ((bitBufferSize > 0) || (memoryBufferPosition < memoryBuffer->size()));
-}
-
-size_t bits_required(size_t maxValue)
-{
-    size_t maxValueForBitCount = 0;
-    for (size_t bitCount = 2; bitCount < MAX_BIT_COUNT; bitCount++)
+    InMemoryBitStream::~InMemoryBitStream()
     {
-        maxValueForBitCount = 0;
-        for (size_t i = 0; i < bitCount; i++)
+        memoryBuffer = nullptr;
+        memoryBufferPosition = 0;
+        bitBufferSize = 0;
+        bitBuffer = 0;
+    }
+
+    void InMemoryBitStream::read_byte_to_bit_buffer()
+    {
+        if (memoryBufferPosition < memoryBuffer->size())
         {
-            maxValueForBitCount |= 1 << i;
+            bitBuffer = memoryBuffer->operator[](memoryBufferPosition++);
+            bitBufferSize = 8;
         }
-        if (maxValue <= maxValueForBitCount)
-            return bitCount;
+        else
+        {
+            always_assert(false && "Out of memory in buffer");
+        }
     }
 
-    always_assert(false && "Max value is too large");
-    return 0;
-}
+    bool InMemoryBitStream::read_bit()
+    {
+        if (bitBufferSize == 0)
+        {
+            read_byte_to_bit_buffer();
+        }
 
-size_t bytes_required(size_t maxValue)
-{
-    size_t bits = bits_required(maxValue);
-    size_t bytes = (bits + 7) / 8;
-    return bytes;
-}
+        --bitBufferSize;
+        bool result = bitBuffer & (1 << bitBufferSize);
+        return result;
+    }
+
+    void InMemoryBitStream::operator>>(bool &bit)
+    {
+        bit = read_bit();
+    }
+
+    bool InMemoryBitStream::can_read() const
+    {
+        return ((bitBufferSize > 0) || (memoryBufferPosition < memoryBuffer->size()));
+    }
+
+    size_t bits_required(size_t maxValue)
+    {
+        size_t maxValueForBitCount = 0;
+        for (size_t bitCount = 2; bitCount < MAX_BIT_COUNT; bitCount++)
+        {
+            maxValueForBitCount = 0;
+            for (size_t i = 0; i < bitCount; i++)
+            {
+                maxValueForBitCount |= 1 << i;
+            }
+            if (maxValue <= maxValueForBitCount)
+                return bitCount;
+        }
+
+        always_assert(false && "Max value is too large");
+        return 0;
+    }
+
+    size_t bytes_required(size_t maxValue)
+    {
+        size_t bits = bits_required(maxValue);
+        size_t bytes = (bits + 7) / 8;
+        return bytes;
+    }
 
 } // namespace azgra
